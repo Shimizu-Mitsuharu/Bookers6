91b464b9dc2598513dc0a48aa92aae62ec30b892
vagrant <vagrant@localhost.localdomain>
2020-06-07 11:44:39 +0000

    initialize Repository

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,:validatable
+
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :follows, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
+  attachment :profile_image, destroy: false
+
+  validates :name, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: {maximum: 50}
+
+  def followed_by?(user)
+    passive_relationships.where(following_id: user.id).exists?
+  end
+end
